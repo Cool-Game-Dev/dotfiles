cfbf995f64efd751d4262884bade3259bf2e84bf
Cool-Game-Dev <c00lgamedev@proton.me>
2025-03-27 19:55:15 -0500

    Refactor Qt theming code

---
+++ modules/home/Apperence/qt.nix
@@ -0,0 +1,30 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
+  options.elysium.apperance.qt.enable = lib.mkEnableOption "Qt" // {
+    default = config.elysium.desktops.enable;
+  };
+  config = {
+    home.packages = with pkgs; [
+      libsForQt5.qtstyleplugin-kvantum
+      libsForQt5.qt5ct
+      candy-icons
+    ];
+
+    qt = {
+      enable = true;
+      platformTheme.name = "qtct";
+      style.name = "kvantum";
+    };
+
+    home.sessionVariables = {
+      QT_QPA_PLATFORMTHEME = "qt5ct"; # Set the Qt platform theme
+      QT_STYLE = "kvantum"; # Set the Qt style
+    };
+  };
+}
