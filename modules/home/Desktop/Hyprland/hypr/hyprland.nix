a7ef21496c9eded061406055302e538225fa46a2
Cool-Game-Dev <c00lgamedev@proton.me>
2025-03-31 18:05:55 -0500

    Change nix language server

--- modules/home/Desktop/Hyprland/hypr/hyprland.nix
+++ modules/home/Desktop/Hyprland/hypr/hyprland.nix
@@ -10,7 +10,7 @@
   wayland.windowManager.hyprland = {
     enable = true;
     plugins = [
-      pkgs.hyprlandPlugins.hypr-dynamic-cursors
+      # pkgs.hyprlandPlugins.hypr-dynamic-cursors Build fail? Check it out later.
     ];
 
     settings = {
