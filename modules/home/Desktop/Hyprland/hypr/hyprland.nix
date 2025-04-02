2bb2b8cfd5b7ec000cf4a54c990f65c2f40bdaec
Cool-Game-Dev <c00lgamedev@proton.me>
2025-04-01 19:09:11 -0500

    Add back build failing package

--- modules/home/Desktop/Hyprland/hypr/hyprland.nix
+++ modules/home/Desktop/Hyprland/hypr/hyprland.nix
@@ -10,7 +10,7 @@
   wayland.windowManager.hyprland = {
     enable = true;
     plugins = [
-      # pkgs.hyprlandPlugins.hypr-dynamic-cursors Build fail? Fix later
+      pkgs.hyprlandPlugins.hypr-dynamic-cursors
     ];
 
     settings = {
