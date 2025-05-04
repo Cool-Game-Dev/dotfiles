3524e2ec237355746f983ab2bd3da11bce59c797
Cool-Game-Dev <c00lgamedev@proton.me>
2025-05-04 15:18:42 -0500

    Install some browsers

--- modules/home/Apps/Internet/zen-browser.nix
+++ modules/home/Apps/Internet/zen-browser.nix
@@ -1,8 +1,9 @@
-{ systemSettings, ... }@inputs:
+{ pkgs, ... }@inputs:
 
 {
   imports = [ inputs.zen-browser.homeModules.beta ];
   
+  home.packages = [ pkgs.firefox pkgs.ungoogled-chromium ];
   programs.zen-browser = {
     enable = true;
 
